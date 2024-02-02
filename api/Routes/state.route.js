0c30c91aae925429eb493c7df4cad59343fbaa5b
GeekBoss24 <142672431+GeekBoss24@users.noreply.github.com>
2024-02-02 12:02:32 +0530

    Update state.route.js

--- api/Routes/state.route.js
+++ api/Routes/state.route.js
@@ -1,8 +1,12 @@
 import express from "express"
-import { addState } from "../Controller/state.controller.js"
+import { addState, getState, getDistrict } from "../Controller/state.controller.js"
 
 const router = express.Router()
 
 router.post("/add-state", addState)
+
+router.get("/get-state", getState)
+
+router.get("/get-district", getDistrict)
 
 export default router
